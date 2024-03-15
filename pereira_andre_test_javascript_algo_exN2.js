181d0fc4a081a1c1f3d42634f698ffa27f8aa32f
LAPTOP-KUTG799N\Utilizador <apereira@grouphh.com>
2024-03-15 14:53:22 +0000

    first commit

---
+++ pereira_andre_test_javascript_algo_exN2.js
@@ -0,0 +1,24 @@
+const uniqueArrayChecker = (inputArray) => {
+  // Validates if it's an array of length 9
+  if (!(Array.isArray(inputArray) && inputArray.length === 9)) return false;
+
+  // Sets don't accept value duplicates
+  let set = new Set();
+
+  // This adds the numbers from the input array to the Set
+  for (let i = 0; i < inputArray.length; i++) {
+    set.add(inputArray[i]);
+  }
+
+  // This checks if the Set has 9 numbers, from 1 to 9
+  for (let i = 1; i <= 9; i++) {
+    if (!set.has(i)) return false;
+  }
+
+  return true;
+};
+
+const validArray = [2, 5, 6, 3, 8, 4, 9, 1, 7];
+const invalidArray = [2, 2, 6, 3, 8, 4, 9, 8, 7];
+
+console.log("Is the input array valid? ", uniqueArrayChecker(invalidArray));
